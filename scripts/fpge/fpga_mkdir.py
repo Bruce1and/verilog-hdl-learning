8cafd5f4c968bd9e69d1afaac39295da518e3e1c
Bruce Land <maggot-web-8s@icloud.com>
2026-07-23 00:42:07 +0800

    updata python script

--- scripts/fpge/fpga_mkdir.py
+++ scripts/fpge/fpga_mkdir.py
@@ -8,14 +8,10 @@ Usage:
 
 from pathlib import Path
 from datetime import date
+import argparse
 import sys
+import subprocess
 
-SUBDIRS = [
-    "rtl",
-    "tb",
-    "sim/bin",
-    "sim/wave",
-]
 
 def get_date():
     return date.today().strftime("%Y-%m-%d")
@@ -25,7 +21,7 @@ def load_template(template_name):
     template_file = template_dir / template_name
 
     if not template_file.exists():
-        print("Missing template.")
+        print(f"Missing template {template_file}.")
         sys.exit(1)
     else:
         return template_file.read_text()
@@ -44,89 +40,206 @@ def render_template(content, module_name):
 
     return content
 
-def create_folders(target_dir):
-    """Create FPGA project folders."""
+def project_base(target_dir, module_name):
 
-    existing_count = 0
-    for sub in SUBDIRS:
-        full_dir = target_dir / sub
-        if not full_dir.exists():
-            full_dir.mkdir(parents = True, exist_ok = True)
-            print(f"Created folder: {full_dir.resolve()}")
-        else:
-            existing_count += 1
-    if existing_count == len(SUBDIRS):
-        print("Your project folders already existed!")
-
-def get_files(module_name):
     return [
         {
-            "folder_name": "rtl",
-            "file_name": f"{module_name}.v",
+            "folder_path": target_dir / "rtl",
+            "file_path": target_dir / "rtl" / f"{module_name}.v",
             "template": render_template(
                 load_template("rtl.v"),
                 module_name
             )
         },
         {
-            "folder_name": "tb",
-            "file_name": f"{module_name}_tb.v",
+            "folder_path": target_dir / "tb",
+            "file_path": target_dir / "tb" / f"{module_name}_tb.v",
             "template": render_template(
                 load_template("tb.v"),
                 module_name
             )
+        },
+        {
+            "folder_path": target_dir / "sim" / "bin",
+            "file_path": target_dir / "sim" / "bin" / f"{module_name}_sim",
+            "template": None
+        },
+        {
+            "folder_path": target_dir / "sim" / "wave",
+            "file_path": target_dir / "sim" / "wave" / f"{module_name}_tb.vcd",
+            "template": None
+        },
+        {
+            "folder_path": None,
+            "file_path": target_dir / "fpga.toml",
+            "template": None
         }
     ]
 
-def create_files(target_dir, module_name):
-    files_to_create = get_files(module_name)
+def create_folder(folder_path):
+    """Create FPGA project folders."""
 
-    created_count = 0
-    for file_info in files_to_create:
-        file_path = target_dir / file_info["folder_name"] / file_info["file_name"]
-        if create_file(
-            file_path,
-            file_info["template"]
-        ):
-            created_count += 1
-    if created_count == 0:
-        print("\nNothing to do.")
-    else:
-        print("\nCompleted.")
+    if folder_path is not None:
+        if folder_path.exists():
+            print(f"Already exists: {folder_path.resolve()}")
+            return
+        folder_path.mkdir(parents = True, exist_ok = True)
+        print(f"Created: {folder_path.resolve()}")
 
 def create_file(file_path, content):
-    if file_path.exists():
-        print(f"{file_path.parent.name} file already exists.")
-        return False
 
+    if file_path.exists():
+        print(f"Already exists: {file_path.resolve()}")
+        return
+    if content is not None:
+        insert_template(file_path, content)
+        print(f"Created: {file_path.resolve()}")
+    else:
+        file_path.touch(exist_ok = True)
+        print(f"Created: {file_path.resolve()}")
+
+def insert_template(file_path, content):
     file_path.write_text(content)
-    print(f"Created file: {file_path.resolve()}")
-    return True
+
+def check_syntax(files_path):
+    v_files = [str(f) for f in files_path.rglob("*.v")]
+    result = subprocess.run([
+        "iverilog", "-g2012", "-Wall", "-t", "null"] + v_files,
+        capture_output = True,
+        text = True
+    )
+
+    if result.returncode == 0:
+        print("Pass\n")
+        for f in v_files:
+            print(f)
+    else:
+        print("False\n")
+        print(result.stdout)
+        print(result.stderr)
+        for f in v_files:
+            print(f)
+
+def find_project_root():
+    current_path = Path.cwd()
+    while current_path != current_path.parent:
+        marker = current_path / "fpga.toml"
+        if marker.exists():
+            return current_path
+        current_path = current_path.parent
+    return None
+
+def compile_files(rtl_path, tb_path, module_name):
+    result = subprocess.run([
+            "iverilog",
+            "-g2012",
+            "-o",
+            f"{module_name}_sim",
+            "null"
+        ]
+        + glob.glob("rtl/*.v"),
+        + glob.glob("tb/*.v"),
+        capture_output = True,
+        text = True
+    )
+    print(result.stdout)
+    print(result.stderr)
+
 
 def main():
+
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest = "command", required = True)
+    """ init parser """
+    init_parser = subparsers.add_parser(
+        "init",
+        help = "Create FPGA project directory structure."
+    )
+    init_parser.add_argument(
+        "module_name",
+        type = str,
+        help = "The name of the module to be created"
+    )
+    init_parser.add_argument(
+        "output_path",
+        nargs = "?",
+        type = Path,
+        default = Path.cwd(),
+        help = "Display the path to the module to be created"
+    )
+
+    """ check parser """
+
+    check_parser = subparsers.add_parser(
+        "check",
+        help = "Skip testbench file generation"
+    )
+
+    """ sim parser """
+
+    compile_parser = subparsers.add_parser(
+        "compile",
+        help = "Skip testbench file generation"
+    )
+    compile_parser.add_argument(
+        "module_name",
+        type = str,
+        help = "The name of the module to be created"
+    )
+    compile_parser.add_argument(
+        "output_path",
+        type = Path,
+        help = "Display the path to the module to be created"
+    )
+
+    """ open wave parser """
+
+    open_wave_parser = subparsers.add_parser(
+        "open_wave",
+        help = "Skip testbench file generation"
+    )
+    open_wave_parser.add_argument(
+        "module_name",
+        type = str,
+        help = "The name of the module to be created"
+    )
+    open_wave_parser.add_argument(
+        "output_path",
+        nargs = "?",
+        type = Path,
+        default = Path.cwd(),
+        help = "Display the path to the module to be created"
+    )
+
     current_dir = Path.cwd()
-    if len(sys.argv) < 2:
-        print("Usage: python fpga_mkdir.py <module name>.")
-        sys.exit(1)
-    if sys.argv[1].isidentifier():
-        module_name = sys.argv[1]
-    else:
-        print("Please enter a valid module name.")
-        sys.exit(1)
-    match len(sys.argv):
-        case 2:
-            target_dir = current_dir / module_name
-        case 3:
-            target_dir = Path(sys.argv[2]) / module_name
-        case _:
-            print("Usage: python fpga_mkdir.py <module name>.")
+    args = parser.parse_args()
+
+    if args.command == "init":
+        if not args.module_name.isidentifier():
+            print("Please enter a valid module name.")
             sys.exit(1)
+        module_name = args.module_name
 
-    print(f"\nModule name: {module_name}")
-    print(f"Project path: {target_dir.resolve()}\n")
+        target_dir = args.output_path / module_name
+        files_info = project_base(target_dir, module_name)
+        for entry in files_info:
+            folder_path = entry["folder_path"]
+            file_path = entry["file_path"]
 
-    create_folders(target_dir)
-    create_files(target_dir, module_name)
+            create_folder(folder_path)
+            if folder_path not in ["wave", "bin"]:
+            # if not file_path.endswith("_sim") or file_path.endswith(".vcd")
+                create_file(file_path, entry["template"])
+    else:
+        project_root = find_project_root()
+        if project_root is None:
+            print("Not a FPGA project.")
+            sys.exit()
+        check_syntax(project_root)
+
+
+
+
 
 if __name__ == "__main__":
     main()
